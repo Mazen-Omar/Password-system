a50a295a7a0e578aa72b2f8e993b2650cf192cb8
Mazen Omar <112568201+Mazen-Omar@users.noreply.github.com>
2024-02-15 05:02:01 +0200

    Delete build/default/debug directory

--- build/default/debug/MCAL_layer/GPIO/hal_gpio.p1.d
+++
@@ -1,8 +0,0 @@
-build/default/debug/MCAL_layer/GPIO/hal_gpio.p1:  \
-MCAL_layer/GPIO/hal_gpio.c  \
-MCAL_layer/GPIO/hal_gpio.h  \
-MCAL_layer/GPIO/../../MCAL_layer/mcal_std_types.h  \
-MCAL_layer/GPIO/../../MCAL_layer/std_libraries.h  \
-MCAL_layer/GPIO/../../MCAL_layer/compiler.h  \
-MCAL_layer/GPIO/../../MCAL_layer/device_config.h  \
-MCAL_layer/GPIO/hal_gpio_cfg.h 
--- build/default/debug/MCAL_layer/device_config.p1.d
+++
@@ -1,3 +0,0 @@
-build/default/debug/MCAL_layer/device_config.p1:  \
-MCAL_layer/device_config.c  \
-MCAL_layer/device_config.h 
